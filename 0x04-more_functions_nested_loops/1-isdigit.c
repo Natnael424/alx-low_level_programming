eb08ca1aa390d48a2769f0a8baeac54f2abca7ee
natnael424 <natnaelwondmagegn424@gmail.com>
2022-10-20 05:20:03 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,7 +4,7 @@
 /**
  * _isdigit - Check if acharacter is a digit
  * @x: The number to be checked
- * Return: 1 for a character that will be a digit or 0for any else
+ * Return: 1 for a character that will be a digit or 0 for any else
  */
 int _isdigit(int x)
 {
